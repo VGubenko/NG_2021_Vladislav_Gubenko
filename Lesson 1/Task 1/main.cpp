391296e9bd3b6050b5c857c717caf8e806a6b2a8
VGubenko <92455996+VGubenko@users.noreply.github.com>
2021-10-13 19:09:45 +0300

    add first task lesson 1

---
+++ Lesson 1/Task 1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name ( "Vlad" );
+    cout << "Hi " << name ;
+    cout << "\n";
+    cout << "How are you ready to code shit today ?";
+
+    return 0;
+}
