1e29780ab2cb7bee9007c18b25633f470f6fcbdb
VGubenko <92455996+VGubenko@users.noreply.github.com>
2021-10-13 23:09:26 +0300

    Add task in for lesson 1

---
+++ Lesson 1/Task 3/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << " given a=50, b=20 " << endl ;
+ int a = 50;
+ int b = 20;
+ a = a + b;
+ b = a - b;
+ a -= b;
+ cout << " after processing " << "a=" << a << " b=" << b;
+
+     return 0;
+}
