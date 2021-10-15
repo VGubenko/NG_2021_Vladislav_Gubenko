9eb11ae4aa9baaf802f1036f0818f28d628ea64c
VGubenko <92455996+VGubenko@users.noreply.github.com>
2021-10-15 14:20:32 +0300

    Fixed style, added cleanup

--- Lesson 1/Task 4/main.cpp
+++ Lesson 1/Task 4/main.cpp
@@ -6,20 +6,35 @@ using namespace std;
 int main()
 {
     float a, b, c, D, x, x1, x2;
-    cout << "Solving a Quadratic Equation" << endl << "Enter the variables a, b, c" << endl;
-    cin >> a >> b >> c;
+    a = 0;
+    b = 0;
+    c = 0;
+    D = 0;
+    x = 0;
+    x1 = 0;
+    x2 = 0;
+
+    cout << "Solving a Quadratic Equation" << endl;
+
+    cout << "Enter the variables a" << endl;
+        cin >> a;
+    cout << "Enter the variables b" << endl;
+        cin >> b;
+    cout << "Enter the variables c" << endl;
+        cin >> c;
+
     D=b*b-4*a*c;
     cout << "The discriminant is " << D << endl;
+
     if (D < 0)
         cout << "The equation has no solutions" << endl;
-    if (D == 0)
 
+    if (D == 0)
         cout << "The equation has one solution" << endl;
         x=-b/(2*a);
         cout << "x=" << x;
 
     if (D > 0)
-
         cout << "The equation has two solutions" << endl;
         x1=(-b+sqrt(D))/(2*a);
         x2=(-b-sqrt(D))/(2*a);
