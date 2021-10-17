4e8705ccbc21c3e19f3753e8736d57cf2b48f6dd
VGubenko <92455996+VGubenko@users.noreply.github.com>
2021-10-17 14:57:41 +0300

    fixing the code style

--- Lesson 1/Task 3/main.cpp
+++ Lesson 1/Task 3/main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main()
 {
-cout << "given a=50, b=20" << endl;
-int a = 50;
-int b = 20;
-    a = a + b;
-    b = a - b;
-    a -= b;
+ cout << "given a=50, b=20" << endl;
+ int a = 50;
+ int b = 20;
+ a = a + b;
+ b = a - b;
+ a -= b;
 
-cout << "after processing" << " " << "a=" << a << " " << "b=" << b;
+ cout << "after processing " << "a=" << a << " b=" << b;
 
-return 0;
+ return 0;
 }
