1b55d63297795ded273dc228d8ed4199fd419348
VGubenko <92455996+VGubenko@users.noreply.github.com>
2021-10-23 17:13:05 +0300

    code optimization

--- Lesson2/Task3/main.cpp
+++ Lesson2/Task3/main.cpp
@@ -5,24 +5,15 @@ using namespace std;
 int main()
 {
  int side;
- cout << "enter size = ";
+ cout << "enter size ";
  cin >> side;
-
- for ( int z = 1; z <= side; z++ )
- {
-    cout << "*";
- }
-    cout << endl;
- for ( int i = 1; i <= side - 2; i++ )
- {
-    cout << "*";
- for ( int j= 1; j <= side - 2; j++ )
- {
-    cout << " ";
- }
-    cout << "*" << endl;
- }
- for ( int y = 1; y <= side; y++ )
-    cout << "*";
-
+ for (int z = 0; z < side; z++) {
+    for (int i = 0; i < side; i++) {
+        if (z != 0 && z != side-1 && i != 0 && i != side-1)
+            cout << " ";
+        else
+            cout << "*";
+        }
+        cout << endl;
+    }
 }
