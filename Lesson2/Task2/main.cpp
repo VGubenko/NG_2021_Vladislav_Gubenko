62269c21f50d4f3f19755de00d9d6fdb9029bcff
VGubenko <92455996+VGubenko@users.noreply.github.com>
2021-10-23 14:54:54 +0300

    completed task 2 of lesson 1

---
+++ Lesson2/Task2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int a, b, c;
+ a = 0;
+ b = 0;
+ c = 0;
+ cout << "enter your number = ";
+ cin >> a;
+ c = a%10;
+ while ( a != 0 )
+    {
+    if ( a%10 == a/10%10 )
+        b += a%10;
+    if ( a < 10 && a == c)
+        b += a;
+    a /= 10;
+    }
+  cout << "hash code of your number " << b;
+}
