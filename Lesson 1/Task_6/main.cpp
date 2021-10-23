ebacf527577ee521ff4df1cc4fc0eff6a0e7ca14
VGubenko <92455996+VGubenko@users.noreply.github.com>
2021-10-23 16:57:53 +0300

    code optimization and copy-paste correction

--- Lesson 1/Task_6/main.cpp
+++ Lesson 1/Task_6/main.cpp
@@ -8,23 +8,21 @@ int main() {
  bool rep = 1;
  cout << "netmask: 255.255.255.";
  cin >> mask;
- while (rep != 0)
-
-    rep = 0;
+ cout << "hosts number: ";
     switch (mask) {
 
-        case 255: cout << "hosts number: " << 1; break;
-        case 254: cout << "hosts number: " << 2; break;
-        case 252: cout << "hosts number: " << 4; break;
-        case 248: cout << "hosts number: " << 8; break;
-        case 240: cout << "hosts number: " << 16; break;
-        case 224: cout << "hosts number: " << 32; break;
-        case 192: cout << "hosts number: " << 64; break;
-        case 128: cout << "hosts number: " << 128; break;
-        case 0: cout << "hosts number: " << 256; break;
+        case 255: cout << 1; break;
+        case 254: cout << 2; break;
+        case 252: cout << 4; break;
+        case 248: cout << 8; break;
+        case 240: cout << 16; break;
+        case 224: cout << 32; break;
+        case 192: cout << 64; break;
+        case 128: cout << 128; break;
+        case 0: cout << 256; break;
+
         default:
             cout << "incorrect number, please entered try again." << endl;
-                rep = 1;
         }
 
 }
